27f1bc5955a6ede94ca2edf66dcf5d8811373489
Harshit Ruwali <harshitruwali@outlook.com>
2020-02-20 23:52:56 +0530

    Update soln.py

--- Qualification_Round_2020/soln.py
+++ Qualification_Round_2020/soln.py
@@ -1,13 +1,67 @@
-def solve((MAX, inputList):
+
+def solve(MAX, inputList):
     currentLibrary = []
     max_score = 0
+    startIndex = []
+    currentValue = []
+
     sum = 0
     startIndex = fullSize   
 
-   libraryindex =[]
-   libraryscanning  = []
+    libraryindex = []
+    libraryscanning = []
 
-   while libray
+    while(len(startIndex)> 0 and len(currentLibrary)>0):
+        startIndex = startIndex - 1
+        for i in range(startIndex, -1, -1):
+
+            currentValue = inputList[i]
+
+            tempSum = sum + currentValue 
+
+            if (tempSum == MAX):  
+                sum = tempSum
+                currentIndexList.append(i) 
+                currentValueList.append(currentValue)
+                break
+
+            elif (tempSum > MAX):
+                continue  
+
+            elif (tempSum < MAX):
+                sum = tempSum
+                currentIndexList.append(i) 
+                currentValueList.append(currentValue) 
+                continue  
+        if (maxScore < sum):
+            maxScore = sum 
+
+            solutionIndexList = []
+            solutionValueList = []
+
+            for y in currentIndexList:
+                solutionIndexList.append(y)  
+            for y in currentValueList:
+                solutionValueList.append(y) 
+
+        if (maxScore == MAX):
+            break 
+
+        if(len(currentValueList) != 0):
+            lastVal = currentValueList.pop()
+            sum = sum - lastVal 
+
+        if(len(currentIndexList) != 0):
+            lastIndex = currentIndexList.pop() 
+            startIndex = lastIndex 
+
+        if(len(currentIndexList) == 0 and (startIndex == 0)): 
+            break
+
+    print("SCORE = " + str(maxScore))  
+
+    return solutionIndexList  
+
 
 def process(fileName):
 
@@ -54,8 +108,8 @@ def process(fileName):
     inputFilesDirectory = "Input/"  
    
 
-fileNames = ["a_example", "b_small", "c_medium",
-             "d_quite_big", "e_also_big"]  
+fileNames = ["a_example", "b_read_on", "c_incunabula",
+             "d_tough_choices", "e_so_many_books", "f_libraries_of_the_world"]  
 
 for fileName in fileNames:  
     process(fileName)
