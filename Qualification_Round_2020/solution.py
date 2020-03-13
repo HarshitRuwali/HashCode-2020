2d7a0e5aa4c28bc756331db80f5aae185b3d2ad3
Harshit Ruwali <harshitruwali@outlook.com>
2020-03-14 00:53:31 +0530

    Working on the solution

--- Qualification_Round_2020/solution.py
+++ Qualification_Round_2020/solution.py
@@ -3,3 +3,22 @@
 import numpy as np
 import tqdm
 import os
+
+class Library():
+	def __init__ (self, total_n_books, singup_time, n_books_day, books):
+		self.total_n_books = total_n_books
+		self.singup_time = singup_time
+		self.n_books_day = n_books_day
+		self.books = books
+
+
+for FILE in os.listdir('.'):
+	if not FILE.endswitch('.txt'):
+		continue
+
+	with open(FILE, r) as ifp:
+		lines = ifp.readlines()
+
+	B, L, D = list(map(int, lines[0].strip().split()))
+
+
